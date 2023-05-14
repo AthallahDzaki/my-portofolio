a454b3eaba2d159b8efb3437f422d3ffab88645d
Athallah Dzaki <discordgg5@gmail.com>
2023-05-14 20:25:43 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './style/Style1.css';
+import './style/Style2.css';
+import './style/App.css';
+import { AppRoutes } from './routes/Routes';
+
+function App() {
+  return (
+    <div className='App'>
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
